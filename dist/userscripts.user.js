06b56cd501e3f4c2f5cdaffb6a446e9a027d6f6e
GyeongSeop Kim <bcong9455@gmail.com>
2026-05-25 07:03:47 +0900

    `

--- dist/userscripts.user.js
+++ dist/userscripts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SOOP (숲) - 사이드바 UI 변경
 // @namespace    https://github.com/bcong
-// @version      20260525065905
+// @version      20260525070333
 // @author       bcong
 // @description  SOOP 사이드바를 커스텀 UI로 대체합니다. 즐겨찾기/인기/추천 채널, 설정 모달, 플레이어 기능 강화.
 // @license      MIT
@@ -13665,12 +13665,16 @@
     "isPinnedStreamWithPinEnabled",
     "isPinnedOnlineOnlyEnabled",
     "isSmallUserLayoutEnabled",
+    "isProfileHidden",
+    "isCategoryHidden",
+    "isProfileForceJpg",
     "isSendLoadBroadEnabled",
     "isDuplicateRemovalEnabled",
     "isTopDuplicateRemovalEnabled",
     "myplusOrder",
     "isChzzkFollowChannelsEnabled",
     "isChzzkTopChannelsEnabled",
+    "isChzzkPinSyncEnabled",
     "displayFollow",
     "displayMyplus",
     "pollIntervalSeconds",
@@ -13900,7 +13904,7 @@
       const el2 = (_a3 = bodyRef.current) == null ? void 0 : _a3.querySelector(`#${id2}`);
       if (el2) el2.scrollIntoView({ behavior: "smooth", block: "start" });
     };
-    const version = (typeof GM_info !== "undefined" ? (_a2 = GM_info == null ? void 0 : GM_info.script) == null ? void 0 : _a2.version : "") || "20260525065905";
+    const version = (typeof GM_info !== "undefined" ? (_a2 = GM_info == null ? void 0 : GM_info.script) == null ? void 0 : _a2.version : "") || "20260525070333";
     const handleExport = async () => {
       const data = {};
       for (const key of EXPORT_KEYS) {
